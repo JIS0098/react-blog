b795899c9732a4467df1e0a80f5ce6e1d86279fb
Jisoo Han <llqrw55@naver.com>
2023-07-29 15:02:49 +0900

    삭제 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-  let [title,setTitle] = useState(0);
-
+  let [title, setTitle] = useState(0);
+  let [입력값 , 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -19,17 +19,28 @@ function App() {
       {글제목.map((a, i) => {
         return (
           <div className='list' key={i}>
-            <h4 onClick={() => { setModal(true); setTitle(i)}}>{글제목[i]}</h4>
-            <span onClick={() => {
+            <h4 onClick={() => { setModal(true); setTitle(i) }}>{글제목[i]}
+            <span onClick={(e) => {
+              e.stopPropagation();
               let copy = [...따봉]
               copy[i] = copy[i] + 1
               따봉변경(copy)
             }}>👍</span>{따봉[i]}
+            </h4>
             <p>2월 17일 발행</p>
           </div>
         )
       })}
 
+    <input type='' onChange={(e)=>{
+      입력값변경(e.target.value);
+    }}/> 
+    <button onClick={()=>{
+      let copy = [...글제목]
+      copy.push(입력값)
+      글제목변경(copy);
+      
+    }} >글작성</button>   
       {
         modal == true ? <Modal title={title} 글제목={글제목} /> : null
       }
@@ -40,10 +51,10 @@ function App() {
   function Modal(props) {
     return (
       <div className='modal'>
-      <h4>{props.글제목[props.title]}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
+        <h4>{props.글제목[props.title]}</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
     )
   }
 }
